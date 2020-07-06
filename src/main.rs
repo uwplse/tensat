09ee1138e8864ed90903caba91c3741f6a3e62d6
Max Willsey <me@mwillsey.com>
2020-07-06 08:53:45 -0700

    Add timing comparison

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
+use std::time::Instant;
 use tamago::{parse::*, verify::*};
 
 fn main() {
-    prove_taso_rules();
+    env_logger::init();
+    prove_taso_rules(false);
+    prove_taso_rules(true);
     // optimize();
 }
 
 fn optimize() {
-  use tamago::model::*;
-  use tamago::rewrites::*;
-  use egg::*;
-  
-  let start = "(input 0 0)".parse().unwrap();
-  let runner = Runner::<Mdl, (), ()>::default().with_expr(&start).run(&rules()[..]);
-  runner.egraph.dot().to_svg("target/tamago.svg").unwrap();
+    use egg::*;
+    use tamago::model::*;
+    use tamago::rewrites::*;
+
+    let start = "(input 0 0)".parse().unwrap();
+    let runner = Runner::<Mdl, (), ()>::default()
+        .with_expr(&start)
+        .run(&rules()[..]);
+    runner.egraph.dot().to_svg("target/tamago.svg").unwrap();
 }
 
-fn prove_taso_rules() {
+fn prove_taso_rules(batch: bool) {
     use std::env::*;
     use std::fs::*;
 
-    env_logger::init();
     let file = args().nth(1).expect("Pls supply taso rules file.");
     let taso_rules = read_to_string(file).expect("Something went wrong reading the file");
 
@@ -27,22 +31,31 @@ fn prove_taso_rules() {
     let initial = parse_rules(&taso_rules);
     println!("Parsed rules!");
 
+    let start = Instant::now();
+    if batch {
+        let mut to_prove = initial.clone();
+        while !to_prove.is_empty() {
+            let n_before = to_prove.len();
+            to_prove = verify(&to_prove);
+            let n_proved = n_before - to_prove.len();
+            println!("Proved {} on this trip", n_proved);
+            if n_proved == 0 {
+                println!("\nCouldn't prove {} rule(s)", to_prove.len());
+                for pair in &to_prove {
+                    let i = initial.iter().position(|p| p == pair).unwrap();
+                    println!("  {}: {} => {}", i, pair.0, pair.1);
+                }
+                break;
+            }
+        }
+    } else {
+        verify_nb(&initial);
+    }
+    let duration = start.elapsed();
 
-    verify_nb(&initial);
-    println!("DONE");
-    // let mut to_prove = initial.clone();
-    // while !to_prove.is_empty() {
-    //     let n_before = to_prove.len();
-    //     to_prove = verify(&to_prove);
-    //     let n_proved = n_before - to_prove.len();
-    //     println!("Proved {} on this trip", n_proved);
-    //     if n_proved == 0 {
-    //         println!("\nCouldn't prove {} rule(s)", to_prove.len());
-    //         for pair in &to_prove {
-    //             let i = initial.iter().position(|p| p == pair).unwrap();
-    //             println!("  {}: {} => {}", i, pair.0, pair.1);
-    //         }
-    //         break;
-    //     }
-    // }
+    println!(
+        "DONE {} in {}",
+        if batch { "batched" } else { "one-at-a-time" },
+        duration.as_secs_f64(),
+    );
 }
