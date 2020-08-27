7620eb82d2a50855ddad0c166bf396658b94dfff
yycdavid <yicheny@google.com>
2020-08-27 22:54:28 +0000

    Experiment scripts

--- run_exp_main.sh
+++ run_exp_main.sh
@@ -1,21 +1,10 @@
 num_passes=5
 node_limit=50000
 iter_limit=15
-time_limit=300
-iter_multi=3
+time_limit=3000
+iter_multi=1
 ilp_time_sec=3600
 
-model=nasrnn
-cargo run -- -r converted.txt -t converted_multi.txt -u -s none --n_iter $iter_limit --no_order --ilp_time_sec $ilp_time_sec --no_cycle --iter_multi $iter_multi --n_sec $time_limit --n_nodes $node_limit --all_weight_only -e ilp -d $model -o tmp/"$model"_"$iter_multi"_stats.txt
-
-model=nasneta
-cargo run -- -r converted.txt -t converted_multi.txt -u -s none --n_iter $iter_limit --no_order --ilp_time_sec $ilp_time_sec --no_cycle --iter_multi $iter_multi --n_sec $time_limit --n_nodes $node_limit --all_weight_only -e ilp -d $model -o tmp/"$model"_"$iter_multi"_stats.txt
-
-model=bert
-cargo run -- -r converted.txt -t converted_multi.txt -u -s none --n_iter $iter_limit --no_order --ilp_time_sec $ilp_time_sec --no_cycle --iter_multi $iter_multi --n_sec $time_limit --n_nodes $node_limit --all_weight_only -e ilp -d $model -o tmp/"$model"_"$iter_multi"_stats.txt
-
-if false
-then
 for pass in $(seq 0 $(expr $num_passes - 1))
 do
     model=resnext50
@@ -39,4 +28,3 @@ do
     model=nasneta
     cargo run -- -r converted.txt -t converted_multi.txt -u -s none --n_iter $iter_limit --no_order --ilp_time_sec 0 --no_cycle --iter_multi $iter_multi --n_sec $time_limit --n_nodes $node_limit --all_weight_only -e ilp -d $model -o tmp/"$model"_"$iter_multi"_stats.txt
 done
-fi
