8028321904a7a3c13559f918543dd106b83fb4c0
Remy Wang <remywang@cs.washington.edu>
2020-09-21 04:46:13 +0000

    Turn on release flag and dump ONNX

--- run_exp_main.sh
+++ run_exp_main.sh
@@ -1,4 +1,4 @@
-num_passes=5
+num_passes=1
 node_limit=50000
 iter_limit=15
 time_limit=3000
@@ -6,14 +6,14 @@ iter_multi=1
 ilp_time_sec=3600
 
 models=(
-    resnext50
+    # resnext50
     bert
-    nasrnn
-    nasneta
+    # nasrnn
+    # nasneta
 )
 
 for model in "${models[@]}"; do
     for pass in $(seq 0 $(expr $num_passes - 1)); do
-        cargo run -- -r converted.txt -t converted_multi.txt -u -s none --n_iter $iter_limit --no_order --ilp_time_sec 0 --no_cycle --iter_multi $iter_multi --n_sec $time_limit --n_nodes $node_limit --all_weight_only -e ilp -d $model -o tmp/"$model"_"$iter_multi"_stats.txt
+        cargo run --release -- -r converted.txt -t converted_multi.txt -u -s none --n_iter $iter_limit --no_order --ilp_time_sec 0 --no_cycle --iter_multi $iter_multi --n_sec $time_limit --n_nodes $node_limit --all_weight_only -e ilp -d $model -o tmp/"$model"_"$iter_multi"_stats.txt
     done
 done
