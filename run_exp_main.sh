234840b2aa65be7d949445fda3799422d307bed7
Remy Wang <remywang@cs.washington.edu>
2020-09-24 03:47:11 +0000

    Compare release and debug

--- run_exp_main.sh
+++ run_exp_main.sh
@@ -14,6 +14,6 @@ models=(
 
 for model in "${models[@]}"; do
     for pass in $(seq 0 $(expr $num_passes - 1)); do
-        cargo run --release -- -r converted.txt -t converted_multi.txt -u -s none --n_iter $iter_limit --no_order --ilp_time_sec 0 --no_cycle --iter_multi $iter_multi --n_sec $time_limit --n_nodes $node_limit --all_weight_only -e ilp -d $model -o tmp/"$model"_"$iter_multi"_stats.txt # -x tmp/"$model"_optimized.model
+        cargo run -- -r converted.txt -t converted_multi.txt -u -s none --n_iter $iter_limit --no_order --ilp_time_sec 0 --no_cycle --iter_multi $iter_multi --n_sec $time_limit --n_nodes $node_limit --all_weight_only -e ilp -d $model -o tmp/"$model"_"$iter_multi"_stats.txt # -x tmp/"$model"_optimized.model
     done
 done
