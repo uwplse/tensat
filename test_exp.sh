e33b6862c79b4dc4e67a8b29a6692ee45452379e
yycdavid <yycdavid25@gmail.com>
2021-03-01 22:14:45 +0000

    Tidy

--- test_exp.sh
+++
@@ -1,9 +0,0 @@
-node_limit=50000
-iter_limit=15
-time_limit=3000
-iter_multi=2
-ilp_time_sec=3600
-
-model=bert
-
-cargo run --release -- -r converted.txt -t converted_multi.txt -u -s none --n_iter $iter_limit --no_order --ilp_time_sec $ilp_time_sec --no_cycle --iter_multi $iter_multi --n_sec $time_limit --n_nodes $node_limit --all_weight_only -e ilp -d $model -o tmp/"$model"_"$iter_multi"_stats.txt
